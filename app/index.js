f1ffb263ee70bae1acf53b0629c5215b3eff27a9
Juan Garcia <juan.gsanchez13@gmail.com>
2023-05-09 21:41:04 +0200

    Modify index function

--- app/index.js
+++ app/index.js
@@ -1,37 +1,46 @@
-import { Character } from "./character.js";
 import { King } from "./king.js";
 import { Fighter } from "./fighter.js";
 import { Counselor } from "./counselor.js";
 import { Squire } from "./squire.js";
 
+const charactersContainer = [];
+
+const myCharacter1 = new King("Joffrey", "Baratheon", 20, 5);
+
+const myCharacter2 = new Fighter("Jamie", "Lannister", 30, "sword", 7);
+
+const myCharacter3 = new Fighter("Daenerys", "Targaryen", 25, "axe", 5);
+
+const myCharacter4 = new Counselor("Tyrion", "Lannister", 35, "Daenerys");
+
+const myCharacter5 = new Squire("Bronn", "Lannister", 40, "Jamie", 10);
+
+charactersContainer.push(
+  myCharacter1,
+  myCharacter2,
+  myCharacter3,
+  myCharacter4,
+  myCharacter5
+);
+
+console.log(myCharacter1.tvShow);
+
 const talk = () => {
-  const charactersContainer = [];
-
-  const myCharacter = new King("Joffrey", "Baratheon", 20, 5);
-
-  const myCharacter2 = new Fighter("Jamie", "Lannister", 30, "sword", 7);
-
-  const myCharacter3 = new Fighter("Daenerys", "Targaryen", 25, "axe", 5);
-
-  const myCharacter4 = new Counselor("Tyrion", "Lannister", 35, "Daenerys");
-
-  const myCharacter5 = new Squire("Bronn", "Lannister", 40, "Jamie", 10);
-
-  charactersContainer.push(
-    myCharacter,
-    myCharacter2,
-    myCharacter3,
-    myCharacter4,
-    myCharacter5
-  );
-  console.log(charactersContainer);
   const messages = [];
-  for (let character = 0; character < charactersContainer.length; character++) {
-    messages.push(charactersContainer[character].communicate);
-  }
-
-  console.log(messages);
+  messages.push(
+    myCharacter1.saySentence(),
+    myCharacter2.saySentence(),
+    myCharacter3.saySentence(),
+    myCharacter4.saySentence(),
+    myCharacter5.saySentence()
+  );
   return messages;
 };
 
 talk();
+
+console.log(talk());
+myCharacter2.endLife();
+myCharacter4.endLife();
+
+console.log(myCharacter2, myCharacter4);
